a3a3595423e06087eb72615ab8d22c5a7e9bac92
Andrei Visan^ <andrei.m.visan@gmail.com>
2024-11-07 21:15:34 +0100

    Added CAP Theorem

--- chash.py
+++ chash.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python3
 
 from hashlib import md5
-from struct import unpack_from
 from typing import List
 
-hash_ring = {}
+hash_ring = [0]
+request_map = {}
 
-def _md5_hash(key: str, num_nodes: int) -> int:
-    # >I - using big endian to get the first 4 bytes
-    hash_int = unpack_from('>I', md5(str(key).encode()).digest())[0]
-    # Choose a prime number close to but larger than num_buckets
-    prime_modulus = next_prime(num_nodes)
-    return round(hash_int % prime_modulus % num_nodes)
+def _md5_hash(key: str) -> int:
+    """Returns a hash value in the range [0, 2^128 - 1]."""
+    hash_value = int(md5(key.encode()).hexdigest(), 16)
+    return hash_value
 
-def next_prime(n):
-    def is_prime(n):
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-    
-    if n < 2:
-        return 2
-    prime = n
-    while not is_prime(prime):
-        prime += 1
-    return prime
+class CacheNode:
+    def __init__(self, identifier):
+        self.identifier = identifier
+        self.key = _md5_hash(self.identifier)
 
-def test_md5_hash(num_keys=10, num_nodes=100):
-    buckets = [0] * num_nodes
-    for i in range(num_keys):
-        bucket = _md5_hash(i, num_nodes)
-        print(bucket)
-        buckets[bucket] += 1
-    # Calculate standard deviation to check uniformity
-    expected = num_keys / num_nodes
-    variance = sum((x - expected) ** 2 for x in buckets) / num_nodes
-    std_dev = variance ** 0.5
-    return std_dev / expected
+def add_node_hash_to_ring(cache_node: CacheNode):
+    global hash_ring
+    while cache_node.key in hash_ring:
+        cache_node.key += 1
+    hash_ring.append(cache_node.key)
+    hash_ring = sorted(hash_ring)
 
-def _add_notes_to_ring(num_keys: int, nodes: List[str]):
-    num_nodes = len(nodes)
-    for i in range(num_nodes):
-        node_hash = _md5_hash(i, num_nodes)
-        hash_ring.setdefault(node_hash, []).append(nodes[i])
-
+def _find_cache_server_for_request(request: str):
+    global request_map
+    request_hash = _md5_hash(request)
+    print(request_hash)
+    def binary_search(ring: List[int], start: int, end: int):
+        if start > end:
+            request_map[hash_ring[start]] = request_hash
+            return
+        mid = start + (end - start) // 2
+        if hash_ring[mid] == request_hash:
+           request_map[hash_ring[mid]] = request_hash
+           return
+        elif hash_ring[mid] < request_hash:
+            binary_search(ring, mid+1, end)
+        else:
+            binary_search(ring, start, mid-1)
+    binary_search(hash_ring, 0, len(hash_ring) - 1)
 
 if __name__ == "__main__":
-    nodes = ['a', 'b', 'c']
-    _add_notes_to_ring(nodes)
+    nodeA = CacheNode("NodeA")
+    nodeB = CacheNode("NodeB")
+    nodeC = CacheNode("NodeC")
+    add_node_hash_to_ring(nodeA)
+    add_node_hash_to_ring(nodeB)
+    add_node_hash_to_ring(nodeC)
     print(hash_ring)
+    req1 = '123/bla'
+    _find_cache_server_for_request(req1)
+    print(request_map) 
